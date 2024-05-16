10fce451835d61d8745ebd5c3468a4b41c91c23f
dsphanduc <phanduc.ds@gmail.com>
2024-05-16 17:16:15 +0700

    chỉnh sửa lần 4

--- resources/js/piechart.js
+++ resources/js/piechart.js
@@ -9,18 +9,6 @@ const svgPie = d3.select("#piechart")
   .attr("width", widthPie)
   .attr("height", heightPie);
 
-
-
-const g = svg.append("g")
-              .attr("transform", `translate(${widths / 2},${heights / 2})`);
-
-// CSV row converter function
-var rowConverter = function (d) {
-  return {
-    type: d.type // Chỉ cần lấy dữ liệu về loại (type) của mỗi bản ghi
-  };
-};
-
 // Dữ liệu Movies và TV shows
 d3.csv("resources/data/netflix_titles.csv")
   .then(function (data) {
@@ -42,34 +30,34 @@ d3.csv("resources/data/netflix_titles.csv")
     console.log(error);
   });
 
-  function draw_pie(data) {
-    // Màu sắc cho các cung của biểu đồ tròn
-    const color = d3.scaleOrdinal(['#FF0000', '#770000']);
-  
-    // Tạo hàm pie để chuyển đổi dữ liệu thành các cung của biểu đồ tròn
-    var pie = d3.pie()
-      .value(function(d) { return d.percent });
-  
-    // Tạo hàm arc để vẽ các cung của biểu đồ tròn
-    const path = d3.arc()
-      .innerRadius(0)
-      .outerRadius(radiusPie);
-  
-    // Chọn tất cả các cung của biểu đồ tròn và áp dụng dữ liệu vào
-    const arc = svgPie.selectAll("arc")
-      .data(pie(data))
-      .enter()
-      .append("g")
-      .attr("class", "arc");
-  
-    // Vẽ các cung của biểu đồ tròn và thêm màu sắc tương ứng
-    arc.append("path")
-      .attr("d", path)
-      .attr("fill", function(d) { return color(d.data.category) });
-  
-    // Hiển thị phần trăm cho mỗi cung
-    arc.append("text")
-      .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
-      .attr("text-anchor", "middle")
-      .text(function(d) { return d.data.category + " (" + d.data.percent.toFixed(1) + "%)"; });
-  }
+function draw_pie(data) {
+  // Màu sắc cho các cung của biểu đồ tròn
+  const color = d3.scaleOrdinal(['#FF0000', '#770000']);
+
+  // Tạo hàm pie để chuyển đổi dữ liệu thành các cung của biểu đồ tròn
+  var pie = d3.pie()
+    .value(function(d) { return d.percent });
+
+  // Tạo hàm arc để vẽ các cung của biểu đồ tròn
+  const path = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
+
+  // Chọn tất cả các cung của biểu đồ tròn và áp dụng dữ liệu vào
+  const arc = svgPie.selectAll("arc")
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .attr("class", "arc");
+
+  // Vẽ các cung của biểu đồ tròn và thêm màu sắc tương ứng
+  arc.append("path")
+    .attr("d", path)
+    .attr("fill", function(d) { return color(d.data.category) });
+
+  // Hiển thị phần trăm cho mỗi cung
+  arc.append("text")
+    .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d.data.category + " (" + d.data.percent.toFixed(1) + "%)"; });
+}
