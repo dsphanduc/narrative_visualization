c096bc19e32957fd630a61620c74a3e6a04eb0a6
dsphanduc <phanduc.ds@gmail.com>
2024-05-18 16:49:02 +0700

    chỉnh sửa lần 10

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,42 +1,52 @@
-document.getElementsByTagName("video")[0].addEventListener('ended', hideVideoDiv);
-document.getElementById('main-btn').addEventListener('click', showContent);
+document.addEventListener("DOMContentLoaded", function() {
+    // Ẩn nội dung ban đầu
+    $('#intro').hide();
+    $('#data-content').hide();
+    $('#footer').hide();
 
-function hideVideoDiv() {
-    document.getElementById("intro-video").style.display = "none";
-    $("#main").fadeIn(2000);
-}
+    // Gắn sự kiện kết thúc video
+    document.getElementsByTagName("video")[0].addEventListener('ended', function() {
+        hideVideoDiv();
+        showContent();
+    });
 
-function showContent() {
-    $('#intro').slideUp();
-    $('#data-content').fadeIn();
-    $('#footer').fadeIn();
-    $('#netflix-logo').animate(
-        {
+    // Gắn sự kiện click cho nút chính
+    document.getElementById('main-btn').addEventListener('click', showContent);
+
+    // Hàm ẩn video và hiển thị nội dung
+    function hideVideoDiv() {
+        document.getElementById("intro-video").style.display = "none";
+        $("#main").fadeIn(2000);
+    }
+
+    function showContent() {
+        $('#intro').slideUp();
+        $('#data-content').fadeIn();
+        $('#footer').fadeIn();
+        $('#netflix-logo').animate({
             width: '10%'
-        }
-        , 'slow');
-    $('#title > h1').animate(
-        {
+        }, 'slow');
+        $('#title > h1').animate({
             fontSize: '50px'
-        }
-        , 'slow');
-    $('#header').addClass('sticky-top');
-}
+        }, 'slow');
+        $('#header').addClass('sticky-top');
+    }
 
-function showInitialPage() {
-    window.scrollTo(0, 0);
-    $('#intro').slideDown();
-    $('#data-content').fadeOut();
-    $('#footer').fadeOut();
-    $('#netflix-logo').animate(
-        {
+    // Hàm hiển thị trang ban đầu
+    function showInitialPage() {
+        window.scrollTo(0, 0);
+        $('#intro').slideDown();
+        $('#data-content').fadeOut();
+        $('#footer').fadeOut();
+        $('#netflix-logo').animate({
             width: '70%'
-        }
-        , 'slow');
-    $('#title > h1').animate(
-        {
+        }, 'slow');
+        $('#title > h1').animate({
             fontSize: '80px'
-        }
-        , 'slow');
-    $('#header').removeClass('sticky-top');
-}
+        }, 'slow');
+        $('#header').removeClass('sticky-top');
+    }
+
+    // Gọi hàm để hiển thị trang ban đầu
+    showInitialPage();
+});
