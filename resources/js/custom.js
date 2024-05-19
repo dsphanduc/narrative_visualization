75c243c1567d69766a7d6e3fcaa6f7f4876b267a
dsphanduc <phanduc.ds@gmail.com>
2024-05-19 15:28:01 +0700

    chỉnh sửa lần 10

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,52 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Ẩn nội dung ban đầu
-    $('#intro').hide();
-    $('#data-content').hide();
-    $('#footer').hide();
+
 
     // Gắn sự kiện kết thúc video
-    document.getElementsByTagName("video")[0].addEventListener('ended', function() {
-        hideVideoDiv();
-        showContent();
-    });
+    document.getElementsByTagName("video")[0].addEventListener('ended', hideVideoDiv);
 
     // Gắn sự kiện click cho nút chính
-    document.getElementById('main-btn').addEventListener('click', showContent);
-
+    document.getElementById('main-btn').addEventListener('click', function() {
+        window.location.href = 'scene1.html'; // Redirect to scene1.html
+    });
+    
     // Hàm ẩn video và hiển thị nội dung
     function hideVideoDiv() {
         document.getElementById("intro-video").style.display = "none";
-        $("#main").fadeIn(2000);
+        document.getElementById("main").style.display = "block"; // Hiển thị phần #main
     }
 
-    function showContent() {
-        $('#intro').slideUp();
-        $('#data-content').fadeIn();
-        $('#footer').fadeIn();
-        $('#netflix-logo').animate({
-            width: '10%'
-        }, 'slow');
-        $('#title > h1').animate({
-            fontSize: '50px'
-        }, 'slow');
-        $('#header').addClass('sticky-top');
-    }
+    
+   
 
-    // Hàm hiển thị trang ban đầu
-    function showInitialPage() {
-        window.scrollTo(0, 0);
-        $('#intro').slideDown();
-        $('#data-content').fadeOut();
-        $('#footer').fadeOut();
-        $('#netflix-logo').animate({
-            width: '70%'
-        }, 'slow');
-        $('#title > h1').animate({
-            fontSize: '80px'
-        }, 'slow');
-        $('#header').removeClass('sticky-top');
-    }
-
-    // Gọi hàm để hiển thị trang ban đầu
-    showInitialPage();
-});
