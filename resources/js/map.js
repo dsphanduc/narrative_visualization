369174bbf33d2f023502ecdf5fb1ff7a1b461a12
dsphanduc <phanduc.ds@gmail.com>
2024-05-20 16:38:14 +0700

    chinh sua lan 12

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,11 +1,11 @@
-import { dataPath, dataSplit, geoDataPath, getUniqueCountries, getUniqueGenres } from "./utils.js";
+import {dataPath, dataSplit, geoDataPath, getUniqueCountries, getUniqueGenres} from "./utils.js";
 
 function makeMap([geoData, data]) {
 
     let selectedYear = null;
     let selectedGenres = null;
 
-    // Transform the list-like elements of each object in the data into an array of elements
+    // Transform the list like elements of each object in the data into an array of elements
     data = dataSplit(data);
 
     // Create the map
@@ -32,45 +32,60 @@ function makeMap([geoData, data]) {
 
     // add the layers to the map
     OSM.addTo(map);
-    CartoDB_PositronNoLabels.addTo(map);
-    Stamen_TonerHybrid.addTo(map);
+    CartoDB_PositronNoLabels.addTo(map)
+    Stamen_TonerHybrid.addTo(map)
 
     // Get an array that contains all countries from the dataset (unique)
-    let uniqueCountries = getUniqueCountries(data);
+    let uniqueCountries = getUniqueCountries(data)
 
-    // Get an array of unique genres of the dataset
-    let uniqueGenres = getUniqueGenres(data);
+    // Get an array of unique genres of the data set
+    let uniqueGenres = getUniqueGenres(data)
 
-    // Movie and Show count for all countries in a given year
+    // Movie and Show count for all countries in a give year
     const countPerCountryPerFilter = function() {
         let tempData = data;
 
         // filter entries of the current year
-        if (selectedYear !== null) tempData = tempData.filter(el => new Date(el.date_added).getFullYear() === selectedYear);
+        if (selectedYear !== null) tempData = tempData.filter(el => new Date(el.date_added).getFullYear() === selectedYear)
 
         // filter entries that contain at least one of the selected genres
-        if (selectedGenres !== null) {
-            tempData = tempData.filter(el => {
-                return selectedGenres.some(genre => el.listed_in.includes(genre));
-            });
-        }
+        if (selectedGenres !== null) tempData = tempData.filter(el => {
+            return [selectedGenres, el.listed_in].reduce((a, b) => a.filter(c => b.includes(c))).length > 0;
+        })
 
         // Count shows and movies per country: {country1: {showCount: 1, movieCount: 2}, ...}
-        return tempData.reduce((obj, el) => {
-            el.country.forEach(d => {
-                if (!obj[d]) obj[d] = { showCount: 0, movieCount: 0 };
-                obj[d][el.type === "Movie" ? "movieCount" : "showCount"] += 1;
-            });
-            return obj;
-        }, {});
-    };
+        return tempData
+            .reduce((obj, el) => {
+                if (el.type === "Movie") {
+                    el.country.forEach((d) => {
+                        if (obj.hasOwnProperty(d)) {
+                            obj[d].hasOwnProperty("movieCount") ? obj[d]["movieCount"] += 1 : obj[d]["movieCount"] = 1
+                        } else {
+                            obj[d] = {"movieCount": 1, "showCount": 0}
+                        }
+                    })
+                }
+                else if (el.type === "TV Show") {
+                    el.country.forEach((d) => {
+                        if (obj.hasOwnProperty(d)) {
+                            obj[d].hasOwnProperty("showCount") ? obj[d]["showCount"] += 1 : obj[d]["showCount"] = 1
+                        } else {
+                            obj[d] = {"showCount": 1, "movieCount": 0}
+                        }
+                    })
+                }
+                return obj
+            }, {})
+    }
 
-    // Create an Object of the form {country1: [north, east], country2: [north, east], ...} for the countries in uniqueCountries
-    let coordinates = geoData.filter(el => uniqueCountries.includes(el.country))
+    // Create an Object of the form {country1: [north, east], country2: [north, east], ...} for the countries
+    //  in uniqueCountries
+    let coordinates = geoData
+        .filter(el => uniqueCountries.includes(el.country))
         .reduce((obj, el) => {
-            obj[el.country] = [el.latitude, el.longitude];
-            return obj;
-        }, {});
+            obj[el.country] = [el.latitude, el.longitude]
+            return obj
+        }, {})
 
     // Create a feature group for the markers
     let circleLayer = L.featureGroup();
@@ -80,22 +95,22 @@ function makeMap([geoData, data]) {
 
         // Remove the previously added circles
         if (map.hasLayer(circleLayer)) {
-            map.removeLayer(circleLayer);
+            map.removeLayer(circleLayer)
         }
 
         // Set a new feature group
         circleLayer = L.featureGroup();
 
         // Get the correctly filtered data set
-        let countData = countPerCountryPerFilter();
+        let countData = countPerCountryPerFilter()
 
         // Stop if there are no values in the filtered data set
-        if (Object.values(countData).length === 0) return;
+        if (Object.values(countData).length === 0) return
 
-        uniqueCountries.forEach(d => {
+        uniqueCountries.forEach((d) => {
 
             // if the country is not present in the given year
-            if (!countData[d]) return;
+            if (!(countData.hasOwnProperty(d))) return
 
             // Add the circle
             let circle = L.circle(coordinates[d], {
@@ -103,82 +118,87 @@ function makeMap([geoData, data]) {
                 fillColor: 'darkred',
                 fillOpacity: 0.5,
                 radius: 500 * (countData[d].movieCount + countData[d].showCount)
-            });
-            circle.addTo(circleLayer);
+            })
+            circle.addTo(circleLayer)
 
             // Add the popup
             let popUp = L.popup();
 
             // define the HTML text that is displayed
-            let content = `<strong>${d}</strong><br>Movies: ${countData[d].movieCount}<br>Shows: ${countData[d].showCount}`;
+            let content = `<strong>${d}</strong><br>Movies: ${countData[d].movieCount}<br>Shows: ${countData[d].showCount}`
 
             // Define the mouseover effect
-            circle.on("mouseover", e => {
+            circle.on("mouseover", (e) => {
                 popUp.setLatLng(e.latlng)
                     .setContent(content)
                     .openOn(map);
-            });
-        });
+            })
+        })
         circleLayer.addTo(map);
-    };
+    }
 
     // Setup the click behaviour of the time slider
     mapTimeSlider.onchange = () => {
         selectedYear = parseInt(mapTimeSlider.value);
-        drawCircles();
-        yearDisplay.innerText = mapTimeSlider.value;
-    };
+        drawCircles()
+        yearDisplay.innerText = mapTimeSlider.value
+    }
 
     // Setup the click behaviour of the timeCheckBox
     mapTimeCheckBox.onclick = () => {
         if (mapTimeCheckBox.checked) {
-            mapTimeSlider.disabled = false;
+            mapTimeSlider.disabled = false
 
             // disable genres and select all genres
-            genreSelectBox.disabled = true;
+            genreSelectBox.disabled = true
             selectedGenres = uniqueGenres;
-            Array.from(genreSelectBox.selectedOptions).forEach(i => i.selected = false);
+            Array.from(genreSelectBox.selectedOptions).forEach(i => i.selected = false)
 
             selectedYear = parseInt(mapTimeSlider.value);
-            drawCircles();
-            yearDisplay.innerText = mapTimeSlider.value;
-        } else {
-            mapTimeSlider.disabled = true;
+            drawCircles()
+            yearDisplay.innerText = mapTimeSlider.value
+        }
+        else {
+            mapTimeSlider.disabled = true
             genreSelectBox.disabled = false;
 
             selectedYear = null;
-            drawCircles();
-            yearDisplay.innerText = "All Years";
+            drawCircles()
+            yearDisplay.innerText = "All Years"
         }
-    };
+    }
 
     // create the options for the genre select box
-    uniqueGenres.forEach(g => {
-        let option = document.createElement('option');
-        option.value = g;
-        option.innerText = g;
-        genreSelectBox.appendChild(option);
-    });
+    uniqueGenres.forEach((g) => {
+        let option = document.createElement('option')
+        option.value = g
+        option.innerText = g
+        genreSelectBox.appendChild(option)
+    })
 
     // define the behaviour for when the selected genres change
     genreSelectBox.onchange = () => {
-        const genres = Array.from(genreSelectBox.selectedOptions).map(el => el.value);
-        selectedGenres = genres;
-        drawCircles();
-    };
+        const genres = Array.from(genreSelectBox.selectedOptions).map(el => el.value)
+        selectedGenres = genres
+        drawCircles()
+    }
 
     // draw circles
-    drawCircles();
+    drawCircles()
+
 }
 
 // make controller elements
-const mapTimeCheckBox = d3.select('#map-time-checkbox').node();
-const mapTimeSlider = d3.select('#map-time-slider').node();
-const yearDisplay = d3.select('#map-year-display').node();
-const genreSelectBox = d3.select('#map-genre-select').node();
+const mapTimeCheckBox = document.getElementById('map-time-checkbox');
+const mapTimeSlider = document.getElementById('map-time-slider');
+const yearDisplay = document.getElementById('map-year-display');
+const genreSelectBox = document.getElementById('map-genre-select')
 
-// Load the datasets using D3
-Promise.all([
+// Require the datasets
+let dataPromises = [
     d3.csv(geoDataPath),
     d3.csv(dataPath)
-]).then(makeMap);
+]
+
+// Resolve promises before making the visualisation
+Promise.all(dataPromises).then(makeMap);
